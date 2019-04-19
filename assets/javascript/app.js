ae3ebe5aa220cb6260995e9f4133d7cb8e14c9ca
Joe Thor <joethor1989@gmail.com>
2019-04-19 15:16:40 -0500

    infection works for one tile

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,15 @@
 
 //list of perimeter plots that go clockwise around the grid
-perimeterPlots = [1,2,3,4,5,10,15,20,25,24,23,22,21,16,11,6]
-activePerimeterPlot = 0
-
+let perimeterPlots = [1,2,3,4,5,10,15,20,25,24,23,22,21,16,11,6]
+let activePerimeterPlot = 0
+let turn = 0
+const mapping = {1: 'tomatoes', 2: 'corn', 3: 'lettuce', 4: 'blueberries', 5: 'eggplants', 6: 'wildcard' }
 
 $(document).ready(function () {
 
-    lastPlantClicked = ''
-    plantCounter = 0
-    gameState = 'planting'
+    let lastPlantClicked = ''
+    let plantCounter = 0
+    let gameState = 'planting'
 
     //updates plant
     $('body').on('click', '.plants', function () {
@@ -19,8 +20,8 @@ $(document).ready(function () {
 
     //updates state of plot
     $('body').on('click', '.box', function () {
-        box = $(this).attr('id');
-        planted = $(this).attr('data');
+        let box = $(this).attr('id');
+        let planted = $(this).attr('data');
         console.log(box)
         console.log(planted)
         if ($(this).attr('data') === 'none' && (lastPlantClicked !== '')) {
@@ -28,24 +29,55 @@ $(document).ready(function () {
             $(this).addClass(lastPlantClicked)
             $('#lastSelected').html(lastPlantClicked)
             plantCounter += 1
-            console.log(plantCounter)
+            
             if (plantCounter >= 25) {
                 gameState = 'infecting'
-                console.log(gameState)
-                infection()
+                
+                infection(turn)
             }
         }
     })
 
-
+    $('body').on('click', '#infectionBtn', function () {
+        var rand = 1 + Math.floor(Math.random() * 6);
+        $('#diceRoll').html(rand)
+        
+        infection(turn)
+       
+    })
 
 })
 
-function infection() {
+function infection(passedTurn) {
 
-    
-    infectionPlot = '#plot' + perimeterPlots[activePerimeterPlot]
-    console.log(infectionPlot)
-    $(infectionPlot).addClass('infection')
+    //creates button on the first time this is called
+    if(passedTurn === 0){
+        nextTurnButton = $('<button>')
+        nextTurnButton.attr('type', 'button')
+        nextTurnButton.attr('id', 'infectionBtn')
+        nextTurnButton.addClass('btn')
+        nextTurnButton.addClass('btn-primary')
+        nextTurnButton.html('Next Turn')
+        $('#infectionStatus').append(nextTurnButton)
+    }
+    turn += 1
+
+    var rand = 1 + Math.floor(Math.random() * 6);
+    $('#diceRoll').html(mapping[rand])
+    state = activePerimeterPlot + passedTurn
+
+    activePlot = '#plot' + perimeterPlots[state]
+    if(passedTurn > 0){
+        lastPlot = '#plot' + perimeterPlots[state-1]
+        $(lastPlot).removeClass('active')
+    }
+    console.log(activePlot)
+    $(activePlot).addClass('active')
+    //checks dice roll against the current active plot
+    if(mapping[rand] === $(activePlot).attr('data') || mapping[rand] === 'wildcard') {
+       
+        $(activePlot).removeClass($(activePlot).attr('data'))
+        $(activePlot).addClass('infected')
+    }
 
 }
