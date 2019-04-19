bfef2c5423c719cac09e5b808da2a12e8a3a9ec6
Joe Thor <joethor1989@gmail.com>
2019-04-19 08:35:31 -0500

    infection function begun, logic for perimeter stepping is included

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,23 @@
+
+//list of perimeter plots that go clockwise around the grid
+perimeterPlots = [1,2,3,4,5,10,15,20,25,24,23,22,21,16,11,6]
+activePerimeterPlot = 0
+
+
 $(document).ready(function () {
 
     lastPlantClicked = ''
     plantCounter = 0
     gameState = 'planting'
+
+    //updates plant
     $('body').on('click', '.plants', function () {
         lastPlantClicked = $(this).attr('id');
 
         $('#lastSelected').html(lastPlantClicked);
     })
+
+    //updates state of plot
     $('body').on('click', '.box', function () {
         box = $(this).attr('id');
         planted = $(this).attr('data');
@@ -22,12 +32,20 @@ $(document).ready(function () {
             if (plantCounter >= 25) {
                 gameState = 'infecting'
                 console.log(gameState)
+                infection()
             }
         }
-
-
-
     })
 
 
+
 })
+
+function infection() {
+
+    
+    infectionPlot = '#plot' + perimeterPlots[activePerimeterPlot]
+    console.log(infectionPlot)
+    $(infectionPlot).addClass('infection')
+
+}
