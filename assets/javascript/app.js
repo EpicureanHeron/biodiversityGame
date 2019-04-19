4fa861c6626d2c3e08d9cb4b4eaf50d5e061689a
Joe Thor <joethor1989@gmail.com>
2019-04-19 07:20:20 -0500

    game state added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,33 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     lastPlantClicked = ''
+    plantCounter = 0
+    gameState = 'planting'
     $('body').on('click', '.plants', function () {
         lastPlantClicked = $(this).attr('id');
-       console.log(lastPlantClicked)
 
+        $('#lastSelected').html(lastPlantClicked);
     })
     $('body').on('click', '.box', function () {
         box = $(this).attr('id');
         planted = $(this).attr('data');
         console.log(box)
         console.log(planted)
-        if($(this).attr('data') === 'none'){
-            $(this).attr('data', lastPlantClicked) 
+        if ($(this).attr('data') === 'none') {
+            $(this).attr('data', lastPlantClicked)
             $(this).addClass(lastPlantClicked)
-
+            $('#lastSelected').html(lastPlantClicked)
+            plantCounter += 1
+            console.log(plantCounter)
+            if (plantCounter >= 25) {
+                gameState = 'infecting'
+                console.log(gameState)
+            }
         }
 
 
+
     })
-    
+
 
 })
