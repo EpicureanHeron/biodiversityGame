b74bb583679aa4149c3eeb7e9a3ba25be44525fb
Joe Thor <joethor1989@gmail.com>
2019-04-20 07:59:36 -0500

    cleanded up javascript, infection stage doesn't begin until button pressed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,7 +33,13 @@ $(document).ready(function () {
             if (plantCounter >= 25) {
                 gameState = 'infecting'
 
-                infection(turn)
+                let nextTurnButton = $('<button>')
+                nextTurnButton.attr('type', 'button')
+                nextTurnButton.attr('id', 'infectionBtn')
+                nextTurnButton.addClass('btn')
+                nextTurnButton.addClass('btn-primary')
+                nextTurnButton.html('Next Turn')
+                $('#infectionStatus').append(nextTurnButton)
             }
         }
     })
@@ -50,79 +56,57 @@ $(document).ready(function () {
 
 function infection(passedTurn) {
 
-    //creates button on the first time this is called
-    if (passedTurn === 0) {
-        nextTurnButton = $('<button>')
-        nextTurnButton.attr('type', 'button')
-        nextTurnButton.attr('id', 'infectionBtn')
-        nextTurnButton.addClass('btn')
-        nextTurnButton.addClass('btn-primary')
-        nextTurnButton.html('Next Turn')
-        $('#infectionStatus').append(nextTurnButton)
-    }
     turn += 1
 
     var rand = 1 + Math.floor(Math.random() * 6);
     $('#diceRoll').html(mapping[rand])
     state = activePerimeterPlot + passedTurn
-
     activePlot = '#plot' + perimeterPlots[state]
+
     if (passedTurn > 0) {
         lastPlot = '#plot' + perimeterPlots[state - 1]
         $(lastPlot).removeClass('active')
     }
-    console.log(activePlot)
+
     $(activePlot).addClass('active')
+
     //checks dice roll against the current active plot
     if (mapping[rand] === $(activePlot).attr('data') || mapping[rand] === 'wildcard') {
-
         $(activePlot).removeClass($(activePlot).attr('data'))
         $(activePlot).addClass('infected')
         $(activePlot).attr('infected', 'true')
         spread($(activePlot).attr('id'))
     }
-
 }
 
+
 function spread(infectedDivID) {
- 
-   
-    
+
     let listOfhits = []
-    
     listOfhits.push(infectedDivID)
-    // logic bad Headers, i think the recurssive nature forces it to skip the last few
-    //yeah, the recurssive function actually only triggers off the first true,
-    //so it really could be a list that we are adding the positive hits to, which then we go through and process
-    //could be a while loop, or nested method to handle the spread
+
     for (h = 0; h < listOfhits.length; h++) {
 
         let infectedDivname = '#' + listOfhits[h]
-        console.log('--------------------------test0---------------')
-        console.log(infectedDivname)
-        console.log('--------------------------test0---------------')
+        let xValueOfInfected = $(infectedDivname).attr('x')
+        let yValueOfInfected = $(infectedDivname).attr('y')
 
-
-       let xValueOfInfected = $(infectedDivname).attr('x')
-       let  yValueOfInfected = $(infectedDivname).attr('y')
         for (i = -1; i < 2; i++) {
+
             let yValueofadjacent = parseInt(yValueOfInfected) + i
+
             for (j = -1; j < 2; j++) {
 
                 let xValueofAdjacent = parseInt(xValueOfInfected) + j
                 let attributeLookUp = '[x=' + xValueofAdjacent + '][y=' + yValueofadjacent + ']'
-                console.log(attributeLookUp)
-                console.log($(attributeLookUp).attr('id'))
-                console.log($(infectedDivname).attr('id'))
+
                 if ($(attributeLookUp).attr('data') === $(infectedDivname).attr('data') && $(attributeLookUp).attr('infected') === 'false') {
                     $(attributeLookUp).removeClass($(infectedDivname).attr('data'))
                     $(attributeLookUp).addClass('infected')
                     $(attributeLookUp).attr('infected', 'true')
                     listOfhits.push($(attributeLookUp).attr('id'))
                 }
-
             }
         }
     }
-
 }
